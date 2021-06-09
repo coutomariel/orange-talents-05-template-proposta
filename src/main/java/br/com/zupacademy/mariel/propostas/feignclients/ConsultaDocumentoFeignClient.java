be5b80cff037b2385e46cc735f1de7346aea2efc
Mariel Couto <coutomariel@gmail.com>
2021-06-09 10:28:52 -0300

    Implementação de feign client para comunicação com serviço externo

---
+++ src/main/java/br/com/zupacademy/mariel/propostas/feignclients/ConsultaDocumentoFeignClient.java
@@ -0,0 +1,17 @@
+package br.com.zupacademy.mariel.propostas.feignclients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Component
+@FeignClient(name = "analise-documento", url = "http://localhost:9999/api", path = "/solicitacao")
+public interface ConsultaDocumentoFeignClient {
+
+	@Bean
+	@PostMapping
+	ConsultaDocumentoClientResponse consultaDocumento(@RequestBody ConsultaDocumentoClientRequest request);
+
+}
